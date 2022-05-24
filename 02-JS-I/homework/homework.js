26f39d2845ab5ee33603c70c9381790febcc959d
Zuckmantra <haroldjul15@gmail.com>
2022-05-24 16:36:37 -0500

    Primer test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,6 +1,6 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
-// Crea una variable "string", puede contener lo que quieras:
+// Crea una variable "string", puede contener lo que quieras:hola
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
