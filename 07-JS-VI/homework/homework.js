1d4a78bf3168b16d24a3453dd0e0059462523e03
matti909 <matias.saantiago@gmail.com>
2022-05-27 20:07:10 -0300

    final7

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,6 +4,8 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let name = nombre[0].toUpperCase() + nombre.substring(1);
+  return name;
 }
 
 function invocarCallback(cb) {
@@ -35,7 +37,9 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  
+  array.map((elemento, i) => {
+      cb(array[i]);
+    })
 }
 
 function map(array, cb) {
@@ -43,12 +47,26 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let newArray = [];
+
+  array.map((elemento, i) => {
+    newArray.push(array[i]);
+    newArray[i] = cb(array[i]);
+  })
+  return newArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let narray = [];
+  array.forEach((alemento, i) => {
+    if(array[i][0] === 'a'){
+      narray.push(array[i]);
+    }
+  })
+  return narray;
 }
 
 // No modificar nada debajo de esta línea
