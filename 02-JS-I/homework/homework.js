5488702b2c7cb5ed58a6100cc1de9373ec949e0d
NidiaRosa <rosabittar0118@hotmail.com>
2022-06-19 23:20:18 -0500

    correcciones_6

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -169,12 +169,13 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (numero > 0){
+  if (numero === 0){
+    return false;
+  }else if(numero > 0){
     return 'Es positivo';
-  }else {
+  }else{
     return 'Es negativo';
   }
-  return false;
 }
 
 function agregarSimboloExclamacion(str) {
