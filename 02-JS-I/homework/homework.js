3180d566094a8df444fa04edd9cc59977847ea1b
NidiaRosa <rosabittar0118@hotmail.com>
2022-06-19 23:03:43 -0500

    correcciones_4

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -196,7 +196,8 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola ' + nombre;
+  var NombreCompleto = 'Hola ' + nombre + '!';
+  return NombreCompleto;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
