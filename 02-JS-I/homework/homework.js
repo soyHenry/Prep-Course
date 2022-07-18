8dc928925d63f64377f4855215aa12371294bf71
Arleznic <mogollon12034@gmail.com>
2022-07-18 10:46:01 -0500

    Homewor 1 JS1 Finaly

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -243,7 +243,7 @@ function esVocal (letra) {
   // que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   // Escribe tu código aquí
-  if (letra.length < 1) {
+  if (letra.length === 1) {
     if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
       return ("Es vocal");
     } else {
