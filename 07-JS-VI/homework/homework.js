50cb280680d305c7e88c75a6b3a0a46c3228a90d
juanpblogomezrobles1 <82005724+juanpblogomezrobles1@users.noreply.github.com>
2022-07-30 16:05:56 -0500

    Update homework.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,12 +4,14 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return  nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1);
+
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+
   cb();
 }
 
@@ -17,26 +19,21 @@ function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-  return cb(n1,n2);
+
+  return cb(n1, n2);
 }
+
 function sumarArray(numeros, cb) {
   // Suma todos los números enteros (int/integers) de un array ("numeros")
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
-  // Primera opcion
-  // var numeros = [1,2,3,4,5]
-  // var suma = 0;
-  // for(var i = 0; i < numeros.length; i++) {
-  //   suma = suma + numeros[i];
-  // }
-  // cb(suma);
-
-  // Segunda opcion
+  //Tu código:
   var sumaTotal = numeros.reduce(function(acc, curr) {
     return acc + curr;
   },0);
   cb(sumaTotal);
 }
+
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
