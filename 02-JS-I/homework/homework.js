eab77fe53b086f5bf88c1e5d7a378a4489e7cdd4
PWilX51 <PWilX51>
2022-08-15 16:04:34 -0400

    final

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -173,7 +173,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' + nombre;
+  return 'Hola ' + nombre;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
