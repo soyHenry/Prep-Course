47ed44f6d636ef4cc7ab9a24a6bde674eccd8d9c
MasterFatal <vajj007@msn.com>
2022-08-17 21:18:07 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "string";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 77;
 
 // Crea una variable booleana:
 const nuevoBool = null;
