7d815028f9ffca941271641b1feb5f0b8e5ba163
MasterFatal <vajj007@msn.com>
2022-08-17 21:41:02 -0500

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "string";
 const nuevoNum = 77;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
