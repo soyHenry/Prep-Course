d93cf53cfa2f54c0034d0baa860f520980e31dee
MasterFatal <vajj007@msn.com>
2022-08-21 16:42:32 -0500

    Se ajusta identación

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -18,7 +18,6 @@ const nuevaMultiplicacion = 10 * 4 === 40 ;
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === 1;
 
-
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
