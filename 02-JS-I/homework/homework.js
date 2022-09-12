fec7100e87e413894c10719fffc7df35115cd709
Abrildaine <abrudaine@gmail.com>
2022-09-12 15:53:33 -0300

    Homework JS-1 Done

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,4 +1,4 @@
-// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta.
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
