5c7a5070585ee0bdba3008562e60f92f8d515af9
Erika Vazquez Perez <evazquezperez@jaeoregon.com>
2022-10-11 14:46:54 -0700

    otro

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -227,12 +227,8 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  if(letra.length > 1){
-    return "Dato incorrecto"
-  }
-  if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
-    return "Es vocal"
-  }
+  if(letra.length > 40){return "Dato incorrecto"}
+  if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){return "Es vocal"}
   return "Dato incorrecto"
 
 
