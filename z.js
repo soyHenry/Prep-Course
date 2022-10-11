b87466f5ad6e989f93761a4f48b210f714a3823a
Erika Vazquez Perez <evazquezperez@jaeoregon.com>
2022-10-11 12:26:06 -0700

    pruebas

--- z.js
+++ z.js
@@ -4,5 +4,11 @@ function resta(a,b)       {return a-b}console.log(resta(88,5));
 
 function multiplica(a,b)  {return a*b}console.log(multiplica(25,3));
 
-function cadena (str)     { console.log('hola '); return str} console.log("Tepes"+cadena);
+function cadena (str)     { console.log('hola '); return str} console.log(cadena("Tepes "+"Franco"));
 
+function ifss(a,b){ if (a == b){ return true}else{return false}} console.log(ifss(7,7));
+
+
+function elevar(num){return Math.pow(num,2)}console.log (elevar(5));
+
+function ele(num,expo){ return Math.pow(num , expo); } console.log(ele(5,2));
