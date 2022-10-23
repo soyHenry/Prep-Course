4999dc8a316cf08b2007a6c6f8af5f82f72b3093
Ignacio Amatt <ignacio_amatt@hotmail.com>
2022-10-23 12:35:05 -0300

    typo fix

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -45,7 +45,7 @@ function nuevoUsuario (nombre, email, password) {
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
-  // De lo contratio, devuelve "false"
+  // De lo contrario, devuelve "false"
   // Tu código:
 }
 
