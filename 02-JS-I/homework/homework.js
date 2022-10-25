84b2a6f7d395835d9ebc0adabe02989331bf1850
mesa9 <114639242+mesa9@users.noreply.github.com>
2022-10-25 03:23:23 -0500

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,30 +1,3 @@
-// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
-
-// Crea una variable "string", puede contener lo que quieras:
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-const nuevaString = 'prueba';
-
-
-// Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 5;
-
-// Crea una variable booleana:
-const nuevoBool = true;
-
-
-// Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === 5;
-
-
-// Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * 4 === 40 ;
-
-
-// Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1;
-
-=======
 const nuevaString = 'primera;
 
 // Crea una variable numérica, puede ser cualquier número:
