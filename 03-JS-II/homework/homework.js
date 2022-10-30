5e02c40baf16ec3ed47dc15e70798ae8bc5f6e3f
Franco Ezequiel Ibañez <francoibanez.dev@gmail.com>
2022-10-30 12:51:36 -0300

    Update homework.js
    Para evitar confusiones en las personas que recién arrancan, en vez de indicarle en la línea 83 que retorne "falso", qudaría mejor que indique que retorne "false". 
    Ya que en la línea 82 dice "true" en vez de "verdadero".

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -80,7 +80,7 @@ function operadoresLogicos(num1, num2, num3) {
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
-  // De lo contrario devuelve "falso"
+  // De lo contrario devuelve "false"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
