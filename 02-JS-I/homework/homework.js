4cb4cc422906e6df87f722c17da5192b209a1e80
Mauricio Acevedo <mauro.ace.rios@gmail.com>
2022-11-03 15:30:57 -0600

    termine los ejercicios del homework 1 si revisar

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -217,8 +217,8 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.length > 1 ) return "dato incorrecto"
-  if (letra !== "a" || letra !== "e"|| letra !== "i" || letra !== "o" || letra!== "u") return "dato incorrector"
-  return "es vocal"
+  if (letra === "a" || letra === "e"|| letra === "i" || letra === "o" || letra=== "u") return "es vocal"
+  return "dato incorrector"
 }
 
 
