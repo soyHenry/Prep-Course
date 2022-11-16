922f552fc522ef53dde9192f0fcb86cd0647a312
Joaquin Flores <flores.joaquin7291@gmail.com>
2022-11-16 12:52:46 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = `Hola`;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = `Hola`;
@@ -16,7 +16,7 @@ const nuevaResta = 10 - null === 5;
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === null; 
 
 
 // En los próximos 22 problemas, deberás completar la función.
