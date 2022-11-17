cf28edab7fad01adbb90f463f898c7b9669f65ca
gican <gican@localhost.localdomain>
2022-11-16 22:24:00 -0300

    tumama2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -13,7 +13,7 @@ const nuevoBool = true;
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
