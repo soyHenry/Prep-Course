4699d2ca37355b034c46a5726efa54aacad73f4f
Fede <federiconieto04@gmail.com>
2022-11-29 20:54:00 -0300

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 5;
 
 // Crea una variable booleana:
 const nuevoBool = null;
