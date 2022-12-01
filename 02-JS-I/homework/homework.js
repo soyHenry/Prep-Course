dc2ba022fc8080b160fd8f3a67edf077f422159d
Sebastian Guarin <sebastiang112400@gmail.com>
2022-12-01 01:40:16 -0500

    resolveds

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,18 +35,19 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+    return x + y;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  return x * y;
   
 }
 
